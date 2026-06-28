4538781d87c9e486a6780b18cdc1db1b4dc8d25e
andy <andychen200822@gmail.com>
2026-06-28 11:15:15 -0700

    fix 404 not found issue, declared the html files as multi page entries

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        code: resolve(__dirname, 'code.html'),
+        camera: resolve(__dirname, 'camera.html'),
+      },
+    },
+  },
 })
